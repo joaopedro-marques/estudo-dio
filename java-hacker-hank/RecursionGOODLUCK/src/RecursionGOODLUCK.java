ab2da1290a09b65473be6fecaa73a44f4bbde49a
joaop <joaopedromourao@outlook.com>
2023-11-21 20:58:56 -0300

    HackerRank Recursion

---
+++ java-hacker-hank/RecursionGOODLUCK/src/RecursionGOODLUCK.java
@@ -0,0 +1,35 @@
+public class RecursionGOODLUCK {
+
+    public static int Summation(int n) {
+        if (n <= 0) {
+            return 0;
+        } else {
+            return n + Summation(n - 1);
+        }
+    }
+
+    public static int Factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        } else {
+            return n * Factorial(n-1);
+        }
+    }
+
+    public static int Exponentiation(int n, int p){
+        if (p <= 0){
+            return 1;
+        } else {
+            return n * Exponentiation(n, p - 1); 
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(Summation(3));
+
+        System.out.println(Factorial(4));
+
+        System.out.println(Exponentiation(5, 3));
+
+    }
+}
