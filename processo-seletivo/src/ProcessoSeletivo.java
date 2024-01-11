8e89f59f62d0be2547b4798b28f000cf0e3949b1
joaop <joaopedromourao@outlook.com>
2024-01-11 10:55:18 -0300

    Desafio Repetição e condição java

--- processo-seletivo/src/ProcessoSeletivo.java
+++ processo-seletivo/src/ProcessoSeletivo.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
@@ -16,7 +17,32 @@ public class ProcessoSeletivo {
                  candidatosSelecionados++;
             }
         }
+
+        for(String candidato: candidatos){
+            entrandoEmContato(candidato);
+        }
     }    
+
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu=false;
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if(continuarTentando)
+                tentativasRealizadas++;
+            else
+                System.out.println("Contato Realizado com Sucesso");
+
+        }while(continuarTentando && tentativasRealizadas < 3);
+
+        if(atendeu)
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + " tentativa");
+        else
+            System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + ", NÚMERO MÁXIMO" 
+            + tentativasRealizadas + " REALIZADA!");
+    }
     static double valorPretendido() {
         return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
@@ -28,6 +54,10 @@ public class ProcessoSeletivo {
         for(int indice = 0; indice < candidadtos.length; indice++){
             System.out.println("O candidadto de nº" + (indice+1) + " é o " + candidadtos[indice]);
         }
+    }
+
+    static boolean atender(){
+        return new Random().nextInt(3)==1;
     }
 
     static void analisarCandidato(double salarioPretentido) {
